d01c2dd2ddda5b092f88b83e0bad295f48237701
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-03-08 21:48:54 -0500

    Change to philosphy advice

--- app.js
+++ app.js
@@ -2,34 +2,69 @@ const quote = document.getElementById("advice");
 const titleNum = document.getElementById("title-number");
 const button = document.getElementById("shuffle-button");
 
-// initialize
-let slip_id = Math.floor(Math.random() * (100 - 0)) + 0;
-setAdvice(`https://api.adviceslip.com/advice/${slip_id}`, quote, titleNum);
+let url = "https://philosophy-quotes-api.glitch.me/quotes"
 
-button.addEventListener("click", (e) => {
-  let slip_id = Math.floor(Math.random() * (100 - 0)) + 0;
-  setAdvice(`https://api.adviceslip.com/advice/${slip_id}`, quote, titleNum);
-})
+fetch(url)
+.then(res => res.json())
+.then(data => {
+  let dataSize = data.length;
+  let dataList = data;
+
+  const num = randomNum(0, dataSize);
+  const json = dataList[num];
+  // source, quote
+  setPersonName(json.source);
+  setQuote(json.quote);
 
 
+  // initialize
+  button.addEventListener("click", (e) => {
+    const num = randomNum(0, dataSize);
+    const json = dataList[num];
+    // source, quote
+    setPersonName(json.source);
+    setQuote(json.quote);
+  })
 
+});
 
-
-async function setAdvice(url, quote, titleNum) {
-  let response = await fetch(url);
-  if(response.ok) {
-    let json = await response.json();
-    let slip = json.slip;
-    console.log(slip);
-    let id = slip["id"];
-    console.log(id);
-    let advice = slip.advice;
-    
-    titleNum.textContent = id;
-    quote.textContent = advice;
-  }
-  else {
-    console.log("response fail");
-    return null;
-  }
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max-min)) + min;
 }
+
+function setPersonName(personName) {
+  titleNum.textContent = personName;
+}
+
+function setQuote(quoteStr) {
+  quote.textContent = quoteStr;
+}
+
+
+// initialize
+// let slip_id = Math.floor(Math.random() * (100 - 0)) + 0;
+// setAdvice(`https://api.adviceslip.com/advice/${slip_id}`, quote, titleNum);
+
+// button.addEventListener("click", (e) => {
+//   let slip_id = Math.floor(Math.random() * (100 - 0)) + 0;
+//   setAdvice(`https://api.adviceslip.com/advice/${slip_id}`, quote, titleNum);
+// })
+
+// async function setAdvice(url, quote, titleNum) {
+//   let response = await fetch(url);
+//   if(response.ok) {
+//     let json = await response.json();
+//     let slip = json.slip;
+//     console.log(slip);
+//     let id = slip["id"];
+//     console.log(id);
+//     let advice = slip.advice;
+    
+//     titleNum.textContent = id;
+//     quote.textContent = advice;
+//   }
+//   else {
+//     console.log("response fail");
+//     return null;
+//   }
+// }
